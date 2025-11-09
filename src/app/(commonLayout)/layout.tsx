c63cc5ff2945a750eec5eecd8c32eee6c46b8d5c
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-11-09 17:43:27 +0600

    added auth

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,10 +1,13 @@
+import PublicFooter from "@/components/shared/Footer";
 import PublicNavbar from "@/components/shared/PublicNavbar";
+
 
 const CommonLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <>
     <PublicNavbar/>
       {children}
+    <PublicFooter/>
     </>
   );
 };
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,29 @@
-
-
+import { Hero } from "@/components/modules/Hero";
+import Specialities from "@/components/modules/Specialties";
+import Steps from "@/components/modules/Specialties";
+import Testimonials from "@/components/modules/Steps";
+import TopRatedDoctors from "@/components/modules/TopRatedDoctors";
+import Head from "next/head";
 
 export default function Home() {
   return (
-   <>
-   
-   </>
+    <>
+      <Head>
+        <title>AI-Powered Healthcare - Find Your Perfect Doctor</title>
+        <meta
+          name="description"
+          content="Discover top-rated doctors tailored to your needs with our AI-powered healthcare platform. Get personalized recommendations and book appointments effortlessly."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Hero />
+        <Specialities />
+        <TopRatedDoctors />
+        <Steps />
+        <Testimonials />
+      </main>
+    </>
   );
 }
